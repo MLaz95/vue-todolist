f1d47872f05d4d57171b8ab4964300275ec1e7e9
marco <lazzari.marco.ml@gmail.com>
2024-02-20 15:49:54 +0100

    lista stampa correttamente

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log('test')
+const {createApp} = Vue;
+
+createApp({
+    data() {
+        return {
+
+        toDoArray:[
+            {
+                text: 'Call grandma',
+                done: false,
+            },
+            {
+                text: 'Get groceries',
+                done: false,
+            },
+            {
+                text: 'laundry',
+                done: true,
+            },
+            {
+                text: 'update CV',
+                done: false,
+            },
+            {
+                text: 'book restaurant',
+                done: true,
+            },
+        ],
+
+        }
+    },
+
+
+}).mount("#app");
