b7190544e8bcfd8b3bf9df0de9f5b00788d5f971
marco <lazzari.marco.ml@gmail.com>
2024-02-20 16:31:55 +0100

    milestone 3 completata; input field aggiunge task correttamente

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ createApp({
     data() {
         return {
 
+        // array with all the tasks
         toDoArray:[
             {
                 text: 'Call grandma',
@@ -27,12 +28,25 @@ createApp({
             },
         ],
 
+        // new task, text value gets updated with user input
+        newTask: {
+            text: '',
+            done: false,
+        },
         }
     },
 
     methods: {
         deleteTask(taskIndex){
             this.toDoArray.splice(taskIndex, 1);
+        },
+
+        // uses spread operator to add the current values of newTask
+        // then resets newTask.text to empty
+        addTask(){
+            this.toDoArray.push({...this.newTask});
+
+            this.newTask.text = '';
         }
     }
 
